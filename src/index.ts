83367fad4d2870335a162165c907342c3aa88f88
Joshua Sullivan <joshua.sullivan@atscale.com>
2019-04-24 10:29:06 -0700

    Trying to use parcel

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "./monaco-environment";
+// import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
+
+// var originalModel = monaco.editor.createModel("heLLo world!", "text/plain");
+// var modifiedModel = monaco.editor.createModel("hello orlando!", "text/plain");
+
+// var diffEditor = monaco.editor.createDiffEditor(document.getElementById("container"));
+// diffEditor.setModel({
+// 	original: originalModel,
+// 	modified: modifiedModel
+// });
